c9150a8ce659d425b3706a871b9c9b9543304162
Adeeb Technology Lab <AdeebTechnologyLab@gmail.com>
2025-04-26 02:09:13 +0500

    new

---
+++ Form1.vb
@@ -0,0 +1,129 @@
+﻿Public Class Form1
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+        System.Diagnostics.Process.Start("https://eportal.iub.edu.pk/login")
+    End Sub
+
+    Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
+        System.Diagnostics.Process.Start("https://eportal.iub.edu.pk/eportal/its")
+    End Sub
+
+    Private Sub PictureBox3_Click(sender As Object, e As EventArgs) Handles PictureBox3.Click
+        System.Diagnostics.Process.Start("https://eportal.iub.edu.pk/eportal/admissions")
+    End Sub
+
+    Private Sub PictureBox4_Click(sender As Object, e As EventArgs) Handles PictureBox4.Click
+        System.Diagnostics.Process.Start("https://eportal.iub.edu.pk/eportal/hostelportal")
+    End Sub
+
+    Private Sub PictureBox5_Click(sender As Object, e As EventArgs) Handles PictureBox5.Click
+        System.Diagnostics.Process.Start("https://my.iub.edu.pk/cms")
+    End Sub
+
+    Private Sub PictureBox6_Click(sender As Object, e As EventArgs) Handles PictureBox6.Click
+        System.Diagnostics.Process.Start("https://lms.iub.edu.pk/my/")
+    End Sub
+
+    Private Sub PictureBox7_Click(sender As Object, e As EventArgs) Handles PictureBox7.Click
+        System.Diagnostics.Process.Start("https://my.iub.edu.pk/timetable/publics?filter=class&pdftype=&campus_id=1&timetable_id=163&department=Department+of+Information+Security&sets=BWP-BSINSC-6TH-1M&teacher=&room=&subject=")
+    End Sub
+
+    Private Sub PictureBox8_Click(sender As Object, e As EventArgs) Handles PictureBox8.Click
+        System.Diagnostics.Process.Start("https://drive.google.com/file/d/1uN8A9nVhuYIQD6snZfh28q7L2aI8v2No/view?usp=sharing")
+    End Sub
+
+    Private Sub PictureBox9_Click(sender As Object, e As EventArgs) Handles PictureBox9.Click
+        System.Diagnostics.Process.Start("https://my.iub.edu.pk/security/vehicle")
+    End Sub
+
+    Private Sub PictureBox10_Click(sender As Object, e As EventArgs) Handles PictureBox10.Click
+        System.Diagnostics.Process.Start("https://my.iub.edu.pk/cms/cms/std_documents")
+    End Sub
+
+    Private Sub PictureBox11_Click(sender As Object, e As EventArgs) Handles PictureBox11.Click
+        System.Diagnostics.Process.Start("https://my.iub.edu.pk/scholarship/apply")
+    End Sub
+
+    Private Sub PictureBox12_Click(sender As Object, e As EventArgs) Handles PictureBox12.Click
+        System.Diagnostics.Process.Start("https://my.iub.edu.pk/academics/student/enrollment/course_repeat_challan")
+    End Sub
+
+    Private Sub PictureBox13_Click(sender As Object, e As EventArgs) Handles PictureBox13.Click
+        System.Diagnostics.Process.Start("https://my.iub.edu.pk/cms/student_survey")
+    End Sub
+
+    Private Sub PictureBox14_Click(sender As Object, e As EventArgs) Handles PictureBox14.Click
+        System.Diagnostics.Process.Start("http://mail.google.com/a/iub.edu.pk")
+    End Sub
+
+    Private Sub PictureBox15_Click(sender As Object, e As EventArgs) Handles PictureBox15.Click
+        System.Diagnostics.Process.Start("https://drive.google.com/drive/u/1/my-drive")
+    End Sub
+
+    Private Sub PictureBox16_Click(sender As Object, e As EventArgs) Handles PictureBox16.Click
+        System.Diagnostics.Process.Start("https://salmanadeeb.wixsite.com/livechat")
+    End Sub
+
+    Private Sub PictureBox17_Click(sender As Object, e As EventArgs) Handles PictureBox17.Click
+        System.Diagnostics.Process.Start("https://www.iub.edu.pk/news-update")
+    End Sub
+
+    Private Sub PictureBox18_Click(sender As Object, e As EventArgs) Handles PictureBox18.Click
+        System.Diagnostics.Process.Start("https://www.iub.edu.pk/contact")
+    End Sub
+
+    Private Sub PictureBox19_Click(sender As Object, e As EventArgs) Handles PictureBox19.Click
+        System.Diagnostics.Process.Start("https://eportal.iub.edu.pk/short_courses/std/students/apply")
+    End Sub
+
+    Private Sub PictureBox20_Click(sender As Object, e As EventArgs) Handles PictureBox20.Click
+        System.Diagnostics.Process.Start("https://eportal.iub.edu.pk/meritlists/index.php?p=")
+    End Sub
+
+    Private Sub PictureBox21_Click(sender As Object, e As EventArgs) Handles PictureBox21.Click
+        System.Diagnostics.Process.Start("https://my.iub.edu.pk/academics/student/finance/my_vouchers")
+    End Sub
+
+    Private Sub UpdateToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles UpdateToolStripMenuItem.Click
+        System.Diagnostics.Process.Start("https://www.microsoft.com/store/productId/9PL653TPWDCH?ocid=pdpshare")
+    End Sub
+
+    Private Sub LoginEportalToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles LoginEportalToolStripMenuItem.Click
+        System.Diagnostics.Process.Start("https://eportal.iub.edu.pk/login")
+    End Sub
+
+    Private Sub LoginMyIUBToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles LoginMyIUBToolStripMenuItem.Click
+        System.Diagnostics.Process.Start("https://my.iub.edu.pk/")
+    End Sub
+
+    Private Sub LoginLMSToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles LoginLMSToolStripMenuItem.Click
+        System.Diagnostics.Process.Start("https://lms.iub.edu.pk/login/index.php")
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        Me.Close()
+    End Sub
+
+    Private Sub GuidelineToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles GuidelineToolStripMenuItem.Click
+        System.Diagnostics.Process.Start("https://youtu.be/8gE_mTr3WR4?si=wlp7SiGvBHTLjmWz")
+    End Sub
+
+    Private Sub LiveChatToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles LiveChatToolStripMenuItem.Click
+        System.Diagnostics.Process.Start("https://salmanadeeb.wixsite.com/livechat")
+    End Sub
+
+    Private Sub DeveloperToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles DeveloperToolStripMenuItem.Click
+        System.Diagnostics.Process.Start("https://adeebtechlab.blogspot.com/")
+    End Sub
+
+    Private Sub PolicyToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles PolicyToolStripMenuItem.Click
+        System.Diagnostics.Process.Start("https://privacypolicypak.blogspot.com/2023/09/privacy-policy-effective-date-25-03.html")
+    End Sub
+
+    Private Sub ShareToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ShareToolStripMenuItem.Click
+        System.Diagnostics.Process.Start("https://iubportals.blogspot.com/")
+    End Sub
+
+    Private Sub RatingToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles RatingToolStripMenuItem.Click
+        System.Diagnostics.Process.Start("https://www.microsoft.com/store/productId/9PL653TPWDCH?ocid=pdpshare")
+    End Sub
+End Class
